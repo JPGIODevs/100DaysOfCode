fd71a53f94dc5c942adc10c56dc069c7f4bd86f1
Jack Gio Packham <122364391+JPGIODevs@users.noreply.github.com>
2023-01-12 14:22:28 +0000

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,12 @@
+listofstuff = ["tree","oinge","boberry"]
+def saidTenTimes(inputWord):
+    for i in range(0,10):
+        print(inputWord + "said " + str(i) + " times")
+
+
+for x in listofstuff:
+    print(x)
+
+saidTenTimes("frog")
+
+

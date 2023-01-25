38356c85d1c36a98c7694fbf44e9db80e74826a7
Jack Gio Packham <122364391+JPGIODevs@users.noreply.github.com>
2023-01-25 12:06:52 +0000

    Add files via upload

--- day8.py
+++ day8.py
@@ -19,28 +19,51 @@ char_to_dots = {
 
 #input and initiation function, defined to aid in try-catch scenario
 def init_morse():
-    choice_word = str(input("Please enter a phrase in morse of alphabet to convert: "))
-    choice_opt = int(input("type 0 to convert to morse, 1 to convert to alphabet"))
+    choice_word = str(input("Please enter a phrase in morse of alphabet to convert: ")).upper()
+    choice_opt = int(input("type 0 to convert to morse, 1 to convert to alphabet: "))
 
     #convert to something prettier, try return arguements to aid defensive coding
     morse(choice_opt, choice_word)
 
 def morse(option: int, words: str):
-    
-    if(option == "0"):
-        #todo for loop for conversion
-        print("hello")
+
+    #variable to be returned as conversion
+    final = str("")
+
+    #encrypt
+    if(option == 0):
+
+        for letter in words:
+            
+            if letter != " ":
+                final += char_to_dots[letter] + " "
+                
+
+            else:
+                final += " "
+
+        
+        print(final)
+        
+
+
+    #decrypt
     elif(option == "1"):
         print("hello")
+
+    return(final)
 
 
 while(running == True):
 
-
-    try:
-        print("wip")
-        running = False
-    except:
-        print("wip")
+    printout = init_morse()
+    print(printout)
+    running = False
+    #try:
+        #init_morse()
+        #print(final)
+        #running = False
+    #except:
+        #print("wip")
 
     

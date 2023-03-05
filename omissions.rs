953d1553249956f426f1e4eaa9621c81944c482c
JPGIODevs <jpgiodevelopments@gmail.com>
2023-03-05 11:40:39 +0000

    omissions inclusion

---
+++ omissions.rs
@@ -0,0 +1,9 @@
+fn main() {
+
+    //CHANGE THIS FOR OMISSIONS
+    let days = vec![36,37,38,39,40];
+
+    for n in days{
+        println!("Day {n} was ommited as I was pushing to other projects!");
+    }
+}

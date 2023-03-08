50323adabe773bb7ca4e468c717c6aca6d786b8e
Jack Gio Packham <122364391+JPGIODevs@users.noreply.github.com>
2023-03-08 13:16:42 +0000

    late remissions

---
+++ day41.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    printf("hello world!\n");
+
+    int yourMum = 57;
+    printf("%d \n", yourMum);
+
+    if (yourMum < 55){
+        printf("Your mum is younger than 55\n");
+    } else {
+        printf("Your mum is older that 55\n");
+    }
+
+    for(int i = 0; i < 5; i++){
+        printf("your mum\n");
+    }
+
+    return 0;
+}
--- omissions.rs
+++ omissions.rs
@@ -1,7 +1,7 @@
 fn main() {
 
     //CHANGE THIS FOR OMISSIONS
-    let days = vec![36,37,38,39,40];
+    let days = vec![36,37,38,39,40,41];
 
     for n in days{
         println!("Day {n} was ommited as I was pushing to other projects!");

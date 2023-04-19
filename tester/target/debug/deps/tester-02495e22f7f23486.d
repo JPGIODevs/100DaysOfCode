5c04be4e2c5d6bb2698d946ac6e0af5f50e41f3a
JPGIODevs <jpgiodevelopments@gmail.com>
2023-04-19 11:04:44 +0100

    contributes to day 83

---
+++ tester/target/debug/deps/tester-02495e22f7f23486.d
@@ -0,0 +1,5 @@
+/home/jxck/Desktop/work/100daysofcode/tester/target/debug/deps/tester-02495e22f7f23486: src/main.rs
+
+/home/jxck/Desktop/work/100daysofcode/tester/target/debug/deps/tester-02495e22f7f23486.d: src/main.rs
+
+src/main.rs:
---
+++ tester/target/debug/tester.d
@@ -0,0 +1,1 @@
+/home/jxck/Desktop/work/100daysofcode/tester/target/debug/tester: /home/jxck/Desktop/work/100daysofcode/tester/src/main.rs
